7975163b23be58d44ae364599448612173e7d0dc
sthasq <skdmlfkdla03@gmail.com>
2025-11-27 18:01:38 +0900

    init

---
+++ backend/src/main/java/com/iot2ndproject/mobilityhub/mobility_hub_backend/MobilityHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iot2ndproject.mobilityhub.mobility_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilityHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilityHubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/iot2ndproject/mobilityhub/mobility_hub_backend/MobilityHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot2ndproject.mobilityhub.mobility_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilityHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
